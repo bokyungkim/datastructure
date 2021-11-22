8cdd16d72adbad3932a6085dda1c8bd18671a32b
bokyungkim <ohholyday@naver.com>
2021-11-23 00:07:21 +0900

    array

--- linkedlist/linkedlist.c
+++ linkedlist/linkedlist.c
@@ -6,7 +6,7 @@
 /*   By: bokim <bokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 18:04:16 by bokim             #+#    #+#             */
-/*   Updated: 2021/11/22 21:17:51 by bokim            ###   ########.fr       */
+/*   Updated: 2021/11/23 00:01:48 by bokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,123 +22,105 @@ LinkedList* createLinkedList(){
 }
 
 int addLLElement(LinkedList* pList, int position, ListNode element){
-	ListNode *preNode = NULL;
-	ListNode *newNode = NULL;
 	if (position >= 0 && position <= pList->currentElementCount){
-		newNode = (ListNode*)malloc(sizeof(ListNode));
+		ListNode *newNode = (ListNode *)malloc(sizeof(ListNode));
 		*newNode = element;
 		newNode->pLink = NULL;
-		*preNode = pList->headerNode;
+		ListNode *preNode = &(pList->headerNode);
 		for (int i = 0; i < position; i++)
 		{
 			preNode = preNode->pLink;
 		}
 		newNode->pLink = preNode->pLink;
 		preNode->pLink = newNode;
+		pList->currentElementCount++;
 	}
 	else {
 		fprintf(stderr, "Invalid access\n");
 		exit(1);
 	}
-	pList->currentElementCount++;
+	return (TRUE);
 }
 
 int removeLLElement(LinkedList* pList, int position){
-	
-}
-
-ListNode* getLLElement(LinkedList* pList, int position){
-	ListNode *preNode = NULL;
 	if (position >= 0 && position <= pList->currentElementCount){
-		*preNode = pList->headerNode;
+		ListNode *preNode = &(pList->headerNode);
 		for (int i = 0; i < position; i++){
 			preNode = preNode->pLink;
 		}
-		return (preNode);
+		preNode->pLink = preNode->pLink->pLink;
+		pList->currentElementCount--;
+	}
+	else {
+		fprintf(stderr, "Invalid access\n");
+		exit(1);
+	}
+	return (TRUE);
+}
+
+ListNode* getLLElement(LinkedList* pList, int position){
+	if (position >= 0 && position <= pList->currentElementCount){
+		ListNode *preNode = &(pList->headerNode);
+		for (int i = 0; i < position; i++){
+			preNode = preNode->pLink;
+		}
+		return (preNode->pLink);
+	}
+	else {
+		fprintf(stderr, "Invalid access\n");
+		exit(1);
 	}
 }
 
 void clearLinkedList(LinkedList* pList){
-	
+	pList->headerNode.pLink = 0;
+	pList->currentElementCount = 0;
 }
 
 int getLinkedListLength(LinkedList* pList){
-	
+	return (pList->currentElementCount);
 }
 
 void deleteLinkedList(LinkedList* pList){
-	
+	free(pList);
+	pList = NULL;
 }
 
 void displayLinkedList(LinkedList* pList){
-		int i = 0;
+	int i = 0;
 
 	if (pList != NULL) {
-
 		printf("현재 원소 개수 : %d\n", pList->currentElementCount);
-
 		for (i = 0; i < pList->currentElementCount; i++) {
-
 			printf("[%d], %d\n", i, getLLElement(pList, i)->data);
-
 		}
-
 	}
-
 	else {
-
 		printf("원소가 없습니다.\n");
-
 	}
-
-
-
 }
 
 int	main(){
 	int i = 0;
-
 	int arrayCount = 0;
 
-	LinkedList * pList = NULL;
-
-	ListNode * pNode = NULL;
-
+	LinkedList* pList = NULL;
+	ListNode* pNode = NULL;
 	ListNode node;
-
 	pList = createLinkedList();
 
 	if (pList != NULL) {
-
 		node.data = 1;
-
 		addLLElement(pList, 0, node);
-
 		node.data = 3;
-
 		addLLElement(pList, 1, node);
-
 		node.data = 5;
-
 		addLLElement(pList, 2, node);
-
 		displayLinkedList(pList);
-
-		// removeLLElement(pList, 0);
-
-		// displayLinkedList(pList);
-
-		// deleteLinkedList(pList);
-
+		removeLLElement(pList, 0);
+		displayLinkedList(pList);
+		deleteLinkedList(pList);
 	}
 
 	return 0;
-
-
-	// LinkedList *pList = createLinkedList();
-	// ListNode *element1 = malloc(sizeof(ListNode));
-	// element1->data = 3;
-	// element1->pLink = NULL;
-	// addLLElement(pList, 0, *element1);
-	// displayLinkedList(pList);
 }
