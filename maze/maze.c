c832b8eb564e136d040fc7249c592fa92f5f22b9
bokim <ohholyday@naver.com>
2021-12-01 16:32:25 +0900

    maze

--- maze/maze.c
+++ maze/maze.c
@@ -6,7 +6,7 @@
 /*   By: bokim <bokim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 10:50:27 by bokim             #+#    #+#             */
-/*   Updated: 2021/11/29 10:56:08 by bokim            ###   ########.fr       */
+/*   Updated: 2021/12/01 16:31:20 by bokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,21 @@
 #include "maze.h"
 #include "../linkedstack/linkedstack.h"
 
-void findPath(int mazeArray[HEIGHT][WIDTH], MapPosition startPos, MapPosition endPos, LinkedStack *pStack){
-
-}
-
 int pushLSMapPosition(LinkedStack *pStack, MapPosition data){
-
+	if (!pStack)
+	{
+		fprintf(stderr, "Stack does not exist\n");
+		return (FALSE);
+	}
+	else
+	{
+		MapPosition *newNode = (MapPosition *)malloc(sizeof(MapPosition));
+		*newNode = data;
+		newNode->pLink = pStack->pTopElement;
+		pStack->pTopElement = newNode;
+		pStack->currentElementCount++;
+	}
+	return (TRUE);
 }
 
 void showPath(LinkedStack *pStack, int mazeArray[HEIGHT][WIDTH]){
@@ -27,9 +36,18 @@ void showPath(LinkedStack *pStack, int mazeArray[HEIGHT][WIDTH]){
 }
 
 void printMaze(int mazeArray[HEIGHT][WIDTH]){
-	
+	for (int i = 0; i < HEIGHT; i++){
+		for (int j = 0; j < WIDTH; j++){
+			printf("%d ", mazeArray[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void findPath(int mazeArray[HEIGHT][WIDTH], MapPosition startPos, MapPosition endPos, LinkedStack *pStack){
+
 }
 
 int	main(){
-	
+
 }
--- maze/maze.h
+++ maze/maze.h
@@ -4,4 +4,12 @@
 #define HEIGHT	4
 #define WIDTH	4
 
+typedef struct
+{
+	int	x;
+	int	y;
+	char direction;
+	struct MapPosition* pLink;
+} MapPosition;
+
 #endif
